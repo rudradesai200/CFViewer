2728a02047a4d908f50156ffca1d34c44f31f309
rudradesai200 <rudradesai200@gmail.com>
2022-01-20 00:01:27 +0530

    docker setup

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,5 @@
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (cd cfviewer; python manage.py createsuperuser --no-input)
 fi
-(cd cfviewer; python3 manage.py collectstatic --noinput; gunicorn CFViewer.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd cfviewer; python3 manage.py collectstatic --noinput; python3 manage.py makemigrations; python3 manage.py migrate; python3 manage.py updater; gunicorn CFViewer.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
